87f932a9046a0ac62426d95ec09e48fa2ccaf0f4
SBilousko <s.bilousko@ukr.net>
2022-02-17 23:27:23 +0200

    Forms 1

---
+++ src/components/textField.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextField = ({ label, type, name, value, onChange, error }) => {
+  return (
+    <div className="mb-4">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="form-control is-invalid"
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextField.defaultProps = {
+  type: "text"
+};
+
+TextField.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+export default TextField;
